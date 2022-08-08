4cb43475685440038fde0c522e0dcbeb32564ce5
Diego Marcelino <da200958@alunos.unisanta.br>
2022-08-08 10:20:17 -0300

    Starting to implement Board and Piece

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,12 +1,12 @@
 package application;
 
+import boardgame.Board;
 import boardgame.Position;
 
 public class Program {
     public static void main(String[] args) throws Exception {
 
-        Position pos = new Position(4, 6);
+        Board board = new Board(8, 8);
 
-        System.out.println(pos);
     }
 }
