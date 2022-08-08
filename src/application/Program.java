4a20cd6b1d5b03d4bbb0699bd1cabe9cf2e07954
Diego Marcelino <da200958@alunos.unisanta.br>
2022-08-08 10:09:01 -0300

    First class: Position

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,7 +1,12 @@
 package application;
 
+import boardgame.Position;
+
 public class Program {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+
+        Position pos = new Position(4, 6);
+
+        System.out.println(pos);
     }
 }
